643ad4bf0c495ff3ccbe35e38ca009bbba3def81
unknown <souhail.ziyadi2001@gmail.com>
2022-10-12 16:27:31 +0100

    v2

--- test/test.js
+++ test/test.js
@@ -1,54 +1,66 @@
-//example 1
+
+
+
+// ex3: test unitaires
+
+// 1)
+
+// var assert = require('assert');
+// const expect = require("expect.js");
+
+
+//2) 3)
+
+// describe('', function(n1,n2) {
+// n1= 10;
+// n2= 3;
+// z= n1 - n2;
+// it('the result should return the minus of two numbers',function () {
+//   // expect(z).to.equal(4)
+//   // assert.equal(z,4);
+//   expect(z).to.be(7)
+    
+// })
+
+// return z;
+
+
+// }
+
+// )
+
 
 // var assert = require('assert');
 // describe('Array', function () {
 //   describe('#indexOf()', function () {
-//     it('should return -1 when the value is not present', function () {
-//       assert.equal([1, 2, 3, 4].indexOf(4), -1);
-//     });
+//  it('should return -1 when the value is not present', function () {
+//   assert.equal([1, 2, 3, 4].indexOf(5), -1);
+// }
+//     );
 //   });
 // });
 
-// const expect = require("expect.js");
-// const {
-//   congratulateStudent,
-//   getStubFile,
-//   getStudentCode,
-//   runStudentCode,
-// } = require("./common/techio");
 
-const EXPECTED_RESPONSE = require("./common/expected-response.js");
 
-const CODE_FILE = process.env.CODE_FILE || getStubFile(__filename);
 
-describe("le programme devrait", () => {
-  it(`afficher "please provide a URL" dans la sortie d'erreurs si aucune URL n'est fournie`, async () => {
-    const program = await runStudentCode(CODE_FILE);
-    expect(program.getErrors().join("").trim()).to.be("please provide a URL");
-  });
 
-  it(`afficher la réponse dans la sortie standard, si une URL valide est fournie`, async () => {
-    const program = await runStudentCode(CODE_FILE, {
-      args: ["https://jsonplaceholder.typicode.com/photos/1"],
+
+ function somme(n1,n2) {
+  //  implémantation de la fonction
+var assert = require('assert');
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
     });
-    const response = program.getLogs().join("").trim();
-    expect(JSON.parse(response)).to.eql(EXPECTED_RESPONSE);
-  });
-
-  it(`afficher l'erreur dans la sortie d'erreurs, si une URL inexistante est fournie`, async () => {
-    const program = await runStudentCode(CODE_FILE, {
-      args: ["https://serveur-inexistant.xyz"],
-    });
-    const error = program.getErrors().join("").trim();
-    expect(error).to.match(/^FetchError: /);
-    expect(error).to.match(/ENOTFOUND/);
-  });
-
-  it("utiliser await au lieu de then() et/ou catch() pour récupérer l'erreur de fetch()", async () => {
-    const code = await getStudentCode(CODE_FILE);
-    expect(code).to.match(/await fetch\(/);
-    expect(code).to.not.match(/\.then\(/);
-    expect(code).to.not.match(/\.catch\(/);
-    congratulateStudent();
   });
 });
+}
+// it('double done', function (done) {
+//   // Calling `done()` twice is an error
+//   setImmediate(done);
+//   setImmediate(done);
+// });
+
+
+
